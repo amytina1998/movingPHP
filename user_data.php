dc09527b644b4121895f5d75a529d0ab035fa6d9
Loina7723 <mirror7723@gmail.com>
2020-05-12 23:33:08 +0800

    remove the success word of database connection

--- user_data.php
+++ user_data.php
@@ -8,7 +8,7 @@
 
 	$db_link = @mysqli_connect($db_host, $db_username, $db_password);
 	if (!$db_link) die("資料連結失敗！");
-	else echo "資料連結成功";
+	//else echo "資料連結成功";
 	mysqli_query($db_link, "SET NAMES 'utf8'");
 
 	$seldb = @mysqli_select_db($db_link, "598_movingApp");
