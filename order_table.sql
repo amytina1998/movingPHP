d0c3169b42754c267fad1ac3efb921628fdbf894
Loina7723 <mirror7723@gmail.com>
2020-05-15 22:53:50 +0800

    debug

--- order_table.sql
+++ order_table.sql
@@ -8,7 +8,7 @@ CREATE TABLE `order`(
   `additional`        varchar(300) CHARACTER SET utf8,
   `estimate_worktime` time         CHARACTER SET utf8,
   `fee` int(10),
-  PRIMARY KEY(order_id);
+  PRIMARY KEY(order_id),
   FOREIGN KEY (member_id)
   REFERENCES member(member_id) ON DELETE SET NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
