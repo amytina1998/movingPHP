3df353cb2c301a6ef8c662a3c293e32477e71393
Loina7723 <mirror7723@gmail.com>
2020-05-15 18:58:10 +0800

    add query()

--- data_sql.php
+++ data_sql.php
@@ -1,17 +1,22 @@
 <?php
 
   function all_user_data(){
-    require './connDB.php';
-
     $sql_query = "SELECT * FROM `member`";
-    $result = mysqli_query($db_link, $sql_query);
-    mysqli_close($db_link);
+    $result = query($sql_query);
     return $result;
   }
 
   function user_data(){
     $sql_query = "SELECT * FROM `member` WHERE `member_id` = '1'";
-    return $sql_query;
+    $result = query($sql_query);
+    return $result;
+  }
+
+  function query($sql_query){
+    require './connDB.php';
+    $result = mysqli_query($db_link, $sql_query);
+    mysqli_close($db_link);
+    return $result;
   }
 
 
