81116e3e63adf217222dc33ba4858cd74a1e8eda
Loina7723 <mirror7723@gmail.com>
2020-05-15 23:07:31 +0800

    move sql file

---
+++ table_sql/order_table.sql
@@ -0,0 +1,14 @@
+CREATE TABLE `order`(
+  `order_id`          int(10) NOT NULL AUTO_INCREMENT,
+  `member_id`         int(11),
+  `moving_date`       date,
+  `moving_time`       time,
+  `moveout_address`   varchar(100) CHARACTER SET utf8,
+  `movein_address`    varchar(100) CHARACTER SET utf8,
+  `additional`        varchar(300) CHARACTER SET utf8,
+  `estimate_worktime` time,
+  `fee` int(10),
+  PRIMARY KEY(order_id),
+  FOREIGN KEY (member_id)
+  REFERENCES member(member_id) ON DELETE SET NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
