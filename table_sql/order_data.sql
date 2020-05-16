f38c7f6a61a468e73476994dd92b63a865bd6a67
Loina7723 <mirror7723@gmail.com>
2020-05-16 20:38:36 +0800

    add status

--- table_sql/order_data.sql
+++ table_sql/order_data.sql
@@ -1,2 +1,2 @@
-INSERT INTO  `order`(`member_id`, `moving_date`, `moving_time`, `moveout_address`,  `movein_address`, `additional`, `estimate_worktime`, `fee`) VALUES
-('1', '2020-05-15', '22:45:00', '新北市一號路', '新北市一號路二段', '小心', '03:00:00', 3000);
+INSERT INTO  `order`(`member_id`, `moving_date`, `moving_time`, `moveout_address`,  `movein_address`, `additional`, `estimate_worktime`, `fee`, `status`) VALUES
+('1', '2020-05-15', '22:45:00', '新北市一號路', '新北市一號路二段', '小心', '03:00:00', 3000, `scheduled`);
