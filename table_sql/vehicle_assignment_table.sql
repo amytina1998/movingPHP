8a53e5f333624182087fb8c2371b4b81b3e8f3fd
Loina7723 <mirror7723@gmail.com>
2020-05-16 22:03:00 +0800

    debug

--- table_sql/vehicle_assignment_table.sql
+++ table_sql/vehicle_assignment_table.sql
@@ -4,7 +4,7 @@ CREATE TABLE `vehicle_assignmemt`(
   `num` int(10),
   PRIMARY KEY (order_id, vehicle),
   FOREIGN KEY (order_id)
-  REFERENCES order(order_id) ON DELETE CASCADE,
+  REFERENCES `order`(order_id) ON DELETE CASCADE,
   FOREIGN KEY (vehicle_id)
-  REFERENCES vehicle(vehicle_id) ON DELETE SET NULL
+  REFERENCES `vehicle`(vehicle_id) ON DELETE SET NULL
 )ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
