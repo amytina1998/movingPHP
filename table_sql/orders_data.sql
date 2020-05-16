fbb13d5d2a1741f963bcccfb5dc740d1f2af206f
Loina7723 <mirror7723@gmail.com>
2020-05-16 22:20:05 +0800

    debug: scheduled

--- table_sql/orders_data.sql
+++ table_sql/orders_data.sql
@@ -1,2 +1,2 @@
 INSERT INTO  `orders`(`member_id`, `moving_date`, `moving_time`, `moveout_address`,  `movein_address`, `additional`, `estimate_worktime`, `fee`, `status`) VALUES
-('1', '2020-05-15', '22:45:00', '新北市一號路', '新北市一號路二段', '小心', '03:00:00', 3000, `scheduled`);
+('1', '2020-05-15', '22:45:00', '新北市一號路', '新北市一號路二段', '小心', '03:00:00', 3000, "scheduled");
