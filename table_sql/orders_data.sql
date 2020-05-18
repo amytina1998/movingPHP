38bf80971626e9857ef9ece342d01532f36ea522
Loina7723 <mirror7723@gmail.com>
2020-05-19 01:10:27 +0800

    change date and worktine structure type

--- table_sql/orders_data.sql
+++ table_sql/orders_data.sql
@@ -1,4 +1,4 @@
-INSERT INTO  `orders`(`member_id`, `moving_date`, `moving_time`, `moveout_address`,  `movein_address`, `additional`, `estimate_worktime`, `fee`, `status`) VALUES
-/*('1', '2020-05-15', '22:45:00', '新北市一號路', '新北市一號路二段', '小心', '03:00:00', 3000, "scheduled"),*/
-('2', '2020-05-17', '07:33:00', '台北市二號路', '台北市二號路二段', '有易碎物', '2:00:00', 6000, 'assigned'),
-('1', '2020-05-17', '07:34:00', '桃園市三號路', '桃園市三號路二段' , '有易碎物', '1:00:00', 1000, 'cancel');
+INSERT INTO  `orders`(`member_id`, `moving_date`, `moveout_address`,  `movein_address`, `additional`, `estimate_worktime`, `fee`, `status`) VALUES
+('1', '2020-05-15 22:45:00', '新北市一號路', '新北市一號路二段', '小心', '3', 3000, "scheduled"),
+('2', '2020-05-17 07:33:00', '台北市二號路', '台北市二號路二段', '有易碎物', '2', 6000, 'assigned'),
+('1', '2020-05-17 07:34:00', '桃園市三號路', '桃園市三號路二段' , '有易碎物', '1', 1000, 'cancel');
--- table_sql/orders_table.sql
+++ table_sql/orders_table.sql
@@ -1,12 +1,11 @@
 CREATE TABLE `orders`(
   `order_id`          int(10) NOT NULL AUTO_INCREMENT,
   `member_id`         int(11),
-  `moving_date`       date, /*搬家日期*/
-  `moving_time`       time, /*搬家時間*/
+  `moving_date`       datetime, /*搬家日期*/
   `moveout_address`   varchar(100) CHARACTER SET utf8, /*搬出地址*/
   `movein_address`    varchar(100) CHARACTER SET utf8, /*搬入地址*/
   `additional`        varchar(300) CHARACTER SET utf8, /*補充，注意事項*/
-  `estimate_worktime` time, /*預計工時*/
+  `estimate_worktime` int(3), /*預計工時*/
   `fee` int(10), /*費用*/
   `status` enum('scheduled', 'assigned', 'cancel'), /*訂單狀態*/
   PRIMARY KEY (order_id),
