cf33c2d6b38cb4010790846953d730a45ee9ef86
Loina7723 <mirror7723@gmail.com>
2020-05-21 02:26:39 +0800

    update order_member_today

--- data_sql.php
+++ data_sql.php
@@ -12,6 +12,24 @@
     return $result;
   }
 
+  function order_member($status){
+    $sql_query = "SELECT * FROM `member` NATURAL JOIN `orders` ";
+    if($status) $sql_query .= "WHERE status = '".$status."';";
+    $result = query($sql_query);
+    return $result;
+  }
+
+  function order_member_today(){
+    //使用GMT+8
+    $sql_query = "SELECT * FROM `member` NATURAL JOIN `orders` ";
+    $sql_query .= "WHERE moving_date >= DATE(UTC_TIMESTAMP() + INTERVAL 8 HOUR) ";
+    $sql_query .= "AND moving_date < ";
+    $sql_query .= "DATE(UTC_TIMESTAMP() + INTERVAL 8 HOUR) + INTERVAL 1 DAY ";
+    $sql_query .= "AND (status = 'scheduled' OR status = 'assigned');";
+    $result = query($sql_query);
+    return $result;
+  }
+
   function order_detail($order_id){
     $sql_query = "SELECT * FROM `member` NATURAL JOIN ";
     $sql_query .="(`orders` LEFT OUTER JOIN ";
@@ -22,19 +40,13 @@
     return $result;
   }
 
-  function order_member_today(){
-    //使用GMT+8
-    $sql_query = "SELECT * FROM `member` NATURAL JOIN `orders` ";
-    $sql_query .= "WHERE moving_date >= DATE(UTC_TIMESTAMP() + INTERVAL 8 HOUR) ";
-    $sql_query .= "AND moving_date < ";
-    $sql_query .= "DATE(UTC_TIMESTAMP() + INTERVAL 8 HOUR) + INTERVAL 1 DAY;";
-    $result = query($sql_query);
-    return $result;
-  }
-
-  function order_member($status){
-    $sql_query = "SELECT * FROM `member` NATURAL JOIN `orders` ";
-    if($status) $sql_query .= "WHERE status = '".$status."';";
+  function valuation_detail($order_id){
+    $sql_query = "SELECT * FROM `member` NATURAL JOIN ";
+    $sql_query .= "(`orders` NATURAL JOIN `valuation`) ";
+    $sql_query .= "LEFT OUTER JOIN ";
+    $sql_query .= "(`vehicle_assignment` NATURAL JOIN `vehicle`) ";
+    $sql_query .= "ON orders.order_id = vehicle_assignment.order_id ";
+    $sql_query .= "WHERE order_id = '".$order_id."';";
     $result = query($sql_query);
     return $result;
   }
