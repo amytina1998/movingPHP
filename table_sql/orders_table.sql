aae9d03eaa77497da27537877ea69a6c06def849
Loina7723 <mirror7723@gmail.com>
2020-05-21 01:22:45 +0800

    add memo column in orders and change data of vehicle_assignment

--- table_sql/orders_table.sql
+++ table_sql/orders_table.sql
@@ -5,10 +5,12 @@ CREATE TABLE `orders`(
   `moveout_address`   varchar(100) CHARACTER SET utf8, /*搬出地址*/
   `movein_address`    varchar(100) CHARACTER SET utf8, /*搬入地址*/
   `additional`        varchar(300) CHARACTER SET utf8, /*補充，注意事項*/
+  `memo`              varchar(300) CHARACTER SET utf8, /*備註*/
   `estimate_worktime` int(3), /*預計工時*/
-  `fee` int(10), /*費用*/
-  `status` enum('evaluating', 'scheduled', 'assigned', 'cancel'), /*訂單狀態*/
-  `last_update` timestamp NOT NULL DEFAULT current_timestamp() ON UPDATE current_timestamp(),
+  `fee`               int(10), /*費用*/
+  `status`            enum('evaluating', 'scheduled', 'assigned', 'cancel'), /*訂單狀態*/
+  `last_update`       timestamp NOT NULL DEFAULT current_timestamp()
+  ON UPDATE current_timestamp(), /*更新時間*/
   PRIMARY KEY (order_id),
   FOREIGN KEY (member_id)
   REFERENCES member(member_id) ON DELETE SET NULL
--- table_sql/vehicle_assignment_data.sql
+++ table_sql/vehicle_assignment_data.sql
@@ -1,7 +1,5 @@
 INSERT INTO `vehicle_assignment` (`order_id`, `vehicle_id`, `num`) VALUES
-(1,1,1),
-(2,1,2),
-(3,1,3),
-(4,1,4),
-(5,1,4),
-(6,1,6);
+(2,1,1),
+(4,1,2),
+(5,1,3),
+(10,1,4);
