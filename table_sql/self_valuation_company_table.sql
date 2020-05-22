0b684f9b21578166b542705595e55057cdb229c1
Loina7723 <mirror7723@gmail.com>
2020-05-22 20:22:22 +0800

    debug: PRIMARY KEY(company_id)

--- table_sql/self_valuation_company_table.sql
+++ table_sql/self_valuation_company_table.sql
@@ -2,7 +2,7 @@ CREATE TABLE `self_valuation_company`(
   `self_valuation_id` int(10),
   `company_id` int(10),
   `status` enum('null', 'chosen', 'unchosen'),
-  PRIMARY KEY (self_valuation_id),
+  PRIMARY KEY (self_valuation_id, company_id),
   FOREIGN KEY (company_id)
   REFERENCES company(company_id) ON DELETE CASCADE
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
