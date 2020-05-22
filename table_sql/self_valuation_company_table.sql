1a5927ee1dd5caa059e93cac9be554a5d159c298
Loina7723 <mirror7723@gmail.com>
2020-05-22 20:28:41 +0800

    add FOREIGN KEY(self_id)

--- table_sql/self_valuation_company_table.sql
+++ table_sql/self_valuation_company_table.sql
@@ -3,6 +3,8 @@ CREATE TABLE `self_valuation_company`(
   `company_id` int(10),
   `status` enum('null', 'chosen', 'unchosen'),
   PRIMARY KEY (self_valuation_id, company_id),
+  FOREIGN KEY (self_valuation_id)
+  REFERENCES self_valuation(self_valuation_id) ON DELETE CASCADE,
   FOREIGN KEY (company_id)
   REFERENCES company(company_id) ON DELETE CASCADE
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
