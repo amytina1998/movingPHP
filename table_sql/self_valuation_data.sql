d37b8e4203a337cc48167cb7f1a99133cb5c078e
Loina7723 <mirror7723@gmail.com>
2020-05-23 14:48:49 +0800

    delete self_valuation

--- table_sql/self_valuation_data.sql
+++
@@ -1,4 +0,0 @@
-INSERT INTO `self_valuation` (`order_id`, `contact_time`, `prefer_valuation_time`, `internet_price`) VALUES
-(6, '10:00~12:00', '2020-05-12 08:18:00', 1001),
-(7, '12:00~14:00', '2020-05-15 20:38:00', 2001),
-(11, '14:00~18:00', '2020-05-27 04:22:00', 3001);
