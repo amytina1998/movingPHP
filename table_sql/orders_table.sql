be1982f86f1ebc4090d400d4acbb59dba54708e7
Loina7723 <mirror7723@gmail.com>
2020-05-25 22:54:04 +0800

    status(done)

--- table_sql/orders_table.sql
+++ table_sql/orders_table.sql
@@ -1,7 +1,7 @@
 CREATE TABLE `orders`(
   `order_id`          int(10) NOT NULL AUTO_INCREMENT,
-  `member_id`         int(11),
-  `company_id`        int(10),
+  `member_id`         int(11),  /*客戶id*/
+  `company_id`        int(10),  /*公司id*/
   `moving_date`       datetime, /*搬家日期*/
   `moveout_address`   varchar(100) CHARACTER SET utf8, /*搬出地址*/
   `movein_address`    varchar(100) CHARACTER SET utf8, /*搬入地址*/
@@ -9,7 +9,7 @@ CREATE TABLE `orders`(
   `memo`              varchar(300) CHARACTER SET utf8, /*備註*/
   `estimate_worktime` int(3), /*預計工時*/
   `fee`               int(10), /*費用*/
-  `status`            enum('evaluating', 'scheduled', 'assigned', 'cancel'), /*訂單狀態*/
+  `status`            enum('evaluating', 'scheduled', 'assigned', `done`, 'cancel'), /*訂單狀態*/
   `last_update`       timestamp NOT NULL DEFAULT current_timestamp()
   ON UPDATE current_timestamp(), /*更新時間*/
   PRIMARY KEY (order_id),
