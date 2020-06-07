62c80325a64880801d0bd41df5eca7d119d99343
Loina7723 <mirror7723@gmail.com>
2020-06-07 09:00:54 +0800

    debug

--- functional_sql.php
+++ functional_sql.php
@@ -95,7 +95,7 @@ function change_status($table, $order_id, $status){
 	$sql_query .="WHERE order_id = '".$order_id."';";
 	$result = query($sql_query);
 	if(!strcmp($result, "1")) {
-		$result = update_new($order_id, 'TRUE')
+		$result = update_new($order_id, 'TRUE');
 		if(!strcmp($result, "success")) return "success";
 		else return $result;
 	}
