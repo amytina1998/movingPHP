66178fb9bb296675c333084c422d81dbab63dff1
Loina7723 <mirror7723@gmail.com>
2020-06-12 22:39:24 +0800

    room_id

--- table_sql_0612/all_tables.sql
+++ table_sql_0612/all_tables.sql
@@ -92,10 +92,10 @@ CREATE TABLE `furniture` ( /*家具*/
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 CREATE TABLE`room`( /*房間*/
-	`room_id`  int(10) NOT NULL, /*房間ID*/
-	`order_id` int(10), 	 		 	 /*訂單ID*/
-	`floor`  	 int(10), 				 /*樓層*/
-	`room_name`  	 int(10),  	 	 /*房間名稱*/
+	`room_id`  int(10) NOT NULL AUTO_INCREMENT, /*房間ID*/
+	`order_id` int(10), 	 	/*訂單ID*/
+	`floor`  	 int(10), 		/*樓層*/
+	`room_name`  	 int(10), /*房間名稱*/
 	`room_type`  enum('room', 'livingRoom', 'outdoor'),  /*房間類別*/
 	PRIMARY KEY(room_id),
 	FOREIGN KEY(order_id)
--- table_sql_0612/room.sql
+++ table_sql_0612/room.sql
@@ -1,8 +1,8 @@
 CREATE TABLE`room`( /*房間*/
-	`room_id`  int(10) NOT NULL, /*房間ID*/
-	`order_id` int(10), 	 		 	 /*訂單ID*/
-	`floor`  	 int(10), 				 /*樓層*/
-	`room_name`  	 int(10),  	 	 /*房間名稱*/
+	`room_id`  int(10) NOT NULL AUTO_INCREMENT, /*房間ID*/
+	`order_id` int(10), 	 	/*訂單ID*/
+	`floor`  	 int(10), 		/*樓層*/
+	`room_name`  	 int(10), /*房間名稱*/
 	`room_type`  enum('room', 'livingRoom', 'outdoor'),  /*房間類別*/
 	PRIMARY KEY(room_id),
 	FOREIGN KEY(order_id)
