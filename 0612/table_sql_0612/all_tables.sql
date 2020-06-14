4522fdb185916d1a4972c039b7c641e75612fe12
Loina7723 <mirror7723@gmail.com>
2020-06-14 22:16:37 +0800

    user_data complete

--- 0612/table_sql_0612/all_tables.sql
+++ 0612/table_sql_0612/all_tables.sql
@@ -92,11 +92,11 @@ CREATE TABLE `furniture` ( /*家具*/
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 CREATE TABLE`room`( /*房間*/
-	`room_id`  int(10) NOT NULL AUTO_INCREMENT, /*房間ID*/
-	`order_id` int(10), 	 	/*訂單ID*/
-	`floor`  	 int(10), 		/*樓層*/
-	`room_name`  	 int(10), /*房間名稱*/
-	`room_type`  enum('room', 'livingRoom', 'outdoor'),  /*房間類別*/
+	`room_id`   int(10) NOT NULL AUTO_INCREMENT, /*房間ID*/
+	`order_id`  int(10), 	 	/*訂單ID*/
+	`floor`  	  int(10), 		/*樓層*/
+	`room_name` int(10), /*房間名稱*/
+	`room_type` enum('room', 'livingRoom', 'outdoor'),  /*房間類別*/
 	PRIMARY KEY(room_id),
 	FOREIGN KEY(order_id)
 	REFERENCES orders(order_id) ON DELETE CASCADE
